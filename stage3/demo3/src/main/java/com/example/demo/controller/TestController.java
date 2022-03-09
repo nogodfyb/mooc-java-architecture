809bb8a171a130d458c7c1b282886f8dc5277480
nogodfyb <120626481@qq.com>
2022-03-09 22:31:39 +0800

    update

---
+++ stage3/demo3/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fyb
+ * @since 2022/3/4
+ */
+
+@RestController
+public class TestController {
+
+
+}
