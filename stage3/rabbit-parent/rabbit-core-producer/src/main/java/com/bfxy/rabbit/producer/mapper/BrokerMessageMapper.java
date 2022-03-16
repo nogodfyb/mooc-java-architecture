3960faa5f4f9374e3ab112a490a7c7692228f4e4
nogodfyb <120626481@qq.com>
2022-03-16 22:12:06 +0800

    update

--- stage3/rabbit-parent/rabbit-core-producer/src/main/java/com/bfxy/rabbit/producer/mapper/BrokerMessageMapper.java
+++ stage3/rabbit-parent/rabbit-core-producer/src/main/java/com/bfxy/rabbit/producer/mapper/BrokerMessageMapper.java
@@ -10,9 +10,9 @@ import com.bfxy.rabbit.producer.entity.BrokerMessage;
 
 @Mapper
 public interface BrokerMessageMapper {
-	
+
     int deleteByPrimaryKey(String messageId);
-    
+
     int insert(BrokerMessage record);
 
     int insertSelective(BrokerMessage record);
@@ -21,16 +21,16 @@ public interface BrokerMessageMapper {
 
     int updateByPrimaryKeySelective(BrokerMessage record);
 
-    int updateByPrimaryKeyWithBLOBs(BrokerMessage record);
+    //int updateByPrimaryKeyWithBLOBs(BrokerMessage record);
 
     int updateByPrimaryKey(BrokerMessage record);
-	
+
 	void changeBrokerMessageStatus(@Param("brokerMessageId")String brokerMessageId, @Param("brokerMessageStatus")String brokerMessageStatus, @Param("updateTime")Date updateTime);
 
 	List<BrokerMessage> queryBrokerMessageStatus4Timeout(@Param("brokerMessageStatus")String brokerMessageStatus);
-	
+
 	List<BrokerMessage> queryBrokerMessageStatus(@Param("brokerMessageStatus")String brokerMessageStatus);
-	
+
 	int update4TryCount(@Param("brokerMessageId")String brokerMessageId, @Param("updateTime")Date updateTime);
-	
+
 }
