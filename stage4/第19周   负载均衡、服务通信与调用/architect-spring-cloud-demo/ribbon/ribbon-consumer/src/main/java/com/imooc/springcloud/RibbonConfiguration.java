414a1656b3ddfc2676f07ef5df114762358903a0
nogodfyb <120626481@qq.com>
2022-09-10 21:46:19 +0800

    update

---
+++ stage4/第19周   负载均衡、服务通信与调用/architect-spring-cloud-demo/ribbon/ribbon-consumer/src/main/java/com/imooc/springcloud/RibbonConfiguration.java
@@ -0,0 +1,17 @@
+package com.imooc.springcloud;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 半仙.
+ */
+@Configuration
+//@RibbonClient(name = "eureka-client", configuration = MyRule.class)
+public class RibbonConfiguration {
+
+//    @Bean
+//    public IRule defaultLBStrategy() {
+//        return new RandomRule();
+//    }
+
+}
